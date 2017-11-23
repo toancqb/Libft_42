5e8dbdda5708b161559840053f281d38975011af
Crypt0 <crypt0@localhost.localdomain>
2017-11-23 20:43:08 +0100

    modify ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -26,17 +26,7 @@ static void		ft_check_sign(const char *str, int *i, int *sign)
 		(*i)++;
 }
 
-static int		min_value(const char *s)
-{
-	if (s[0] == '-' && s[1] == '2' && s[2] == '1' && s[3] == '4'
-			&& s[4] == '7' && s[5] == '4' && s[6] == '8' && s[7] == '3'
-			&& s[8] == '6' && s[9] == '4' && s[10] == '8')
-		return (-2147483648);
-	else
-		return (0);
-}
-
-int				ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int i;
 	int res;
@@ -44,15 +34,13 @@ int				ft_atoi(const char *str)
 
 	i = 0;
 	sign = 1;
-	if (min_value(str) != 0)
-		return (min_value(str));
 	ft_check_sign(str, &i, &sign);
 	if (!ft_isdigit(str[i]))
 		return (0);
-	res = (int)str[i++] - '0';
+	res = sign * ((int)str[i++] - '0');
 	while (str[i] != '\0' && ft_isdigit(str[i]))
 	{
-		res = res * 10 + (int)str[i++] - '0';
+		res = res * 10 + sign * ((int)str[i++] - '0');
 	}
-	return (res * sign);
+	return (res);
 }
